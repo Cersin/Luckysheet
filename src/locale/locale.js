2ce73f318b3b844af6eba1e1ddcea2a5ee681bff
Lukasz Przybyslawski <lukasz.przybyslawski@varid.pl>
2022-05-23 13:47:53 +0200

    feat: add base pl locale from en

--- src/locale/locale.js
+++ src/locale/locale.js
@@ -2,9 +2,10 @@ import en from './en'
 import zh from './zh'
 import es from './es'
 import zh_tw from './zh_tw'
+import pl from './pl'
 import Store from '../store';
 
-const localeObj = {en,zh,es,zh_tw}
+const localeObj = {en,zh,es,zh_tw,pl}
 
 function locale(){
     return localeObj[Store.lang];
